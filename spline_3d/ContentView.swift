74bceebe048373b4ba0bca64aaf70e4d18f33e32
Sarim <sarimk80@gmail.com>
2024-11-12 10:28:20 +0500

    Initial Commit

---
+++ spline_3d/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spline_3d
+//
+//  Created by sarim khan on 12/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spline_3d/spline_3dApp.swift
@@ -0,0 +1,17 @@
+//
+//  spline_3dApp.swift
+//  spline_3d
+//
+//  Created by sarim khan on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct spline_3dApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
